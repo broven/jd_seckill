6be21b3d0f6c79a3ecbf218a5029c404d8f7dd18
metajs <zycode277@gmail.com>
2021-01-07 23:36:43 +0800

    update

---
+++ jd/jd_monitor/__init__.py
@@ -0,0 +1,7 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*
+"""
+京东库存监控
+""
+
+# https://item.jd.com/100008245245.html
---
+++ jd/jd_monitor/puppteer.py
@@ -0,0 +1,6 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*
+
+class Puppteer:
+    def __init__(self):
+        
